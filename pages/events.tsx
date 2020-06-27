d2a9ccff110c4ffc748126e60397f812b42f3765
Zoreslav <gzoreslav@gmail.com>
2020-06-27 21:51:31 +0300

    NC-1: Add simple events page with API

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 
-const Events: React.FunctionComponent = () => (
+const Events: React.FunctionComponent = ({ data }) => (
   <div>
     <h1>Events</h1>
     <Link href="/">Home</Link>
-    <p>No events</p>
+    <ul>
+      {data.map(({ title, _id }) => (
+        <li key={`event-${_id}`}>
+          <Link href={`/events/${_id}`}>{title}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 )
 export default Events
+
+export async function getServerSideProps() {
+  const res = await fetch('http://localhost:3000/api/events')
+  const json = await res.json()
+  return { props: { data: json } }
+}
