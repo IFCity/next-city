0397d43ebceeeed6ec497bf94bd5ac28f7aaef04
Zoreslav <gzoreslav@gmail.com>
2020-06-27 20:21:21 +0300

    Check precommit hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Home: React.FunctionComponent = () => (
     <div>
-        <p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p>
+        <p>Hello! </p><p>Hi! </p><p>Hello! </p><p>Hi! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p><p>Hello! </p>
     </div>
 )
 export default Home;
